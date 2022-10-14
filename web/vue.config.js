752247da2c721bbe5f17df253a4b9d498e5fede9
starao520 <starao520@outlook.com>
2022-10-14 23:53:14 +0800

    添加前后端共同部署配置

---
+++ web/vue.config.js
@@ -0,0 +1,26 @@
+const http = '127.0.0.1:8415'
+
+module.exports = {
+    //  输出目录
+    outputDir: '../src/main/resources/dist',
+    //  静态资源使用相对路径
+    publicPath: './',
+    //  代理配置
+    devServer: {
+        '/**': {
+            target: `http://${http}`,
+            timeout: 60 * 1000
+        }
+    },
+    configureWebpack: {
+
+    },
+    chainWebpack: (config) => {
+        config.plugin('html').tap((args) => {
+            args[0].title = "EMS管理系统";
+            args[0].build = new Date().getTime();
+            args[0].env = process.env.NODE_ENV;
+            return args;
+        })
+    }
+}
